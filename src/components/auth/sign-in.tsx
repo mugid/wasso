aad31580879d66465cf527ad1453cc685902d396
mugid <bekslambek22@gmail.com>
2025-06-19 21:52:54 +0500

    fear: create signIn function

--- src/components/auth/sign-in.tsx
+++ src/components/auth/sign-in.tsx
@@ -1,5 +1,11 @@
 import { authClient } from "@/lib/auth-client"
 
+const signIn = async () => {
+    const data = await authClient.signIn.social({
+        provider: "google"
+    })
+}
+
 export function SignIn() {
     return (
         <button className="bg-blue-500 text-white px-4 py-2 rounded">

b66c6d8ccc5f4e3930a585eafb1319d2c6d408cd
mugid <bekslambek22@gmail.com>
2025-07-05 21:21:00 +0500

    fix: switch to azure openai for word generation

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,14 +1,16 @@
 import { generateObject } from "ai";
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+import { createAzure } from "@ai-sdk/azure";
 import { z } from "zod";
 
 export async function getGeneratedWords() {
-  const openrouter = createOpenRouter({
-    apiKey: process.env.OPENROUTER_API_KEY,
+  const azure = createAzure({
+    resourceName: "wasso",
+    apiKey: process.env.NEXT_PUBLIC_AZURE_API_KEY,
   });
 
   const { object } = await generateObject({
-    model: openrouter('cypher-alpha:free'),
+    model: azure('gpt-04-mini'),
     schema: z.object({
       words: z.array(z.string()),
     }),
